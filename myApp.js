2f146cda31ab0a9dbb0467ffe4658f4639d99243
josegutierrezmorals <josegutierrezmorals@gmail.com>
2026-05-05 23:59:02 +0000

    Primer commit

--- myApp.js
+++ myApp.js
@@ -1,61 +1,21 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
 
+// 1. Ocultar Express
+app.use(helmet.hidePoweredBy());
 
+// 2. Mitigar Clickjacking
+app.use(helmet.frameguard({ action: 'deny' }));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
-const api = require('./server.js');
 app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+const api = require('./server.js');
+app.use('/_api', api);
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
+module.exports = app;
