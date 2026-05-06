36dd381b19e01d2331690a0d85fa29c47ac4ddda
josegutierrezmorals <josegutierrezmorals@gmail.com>
2026-05-06 01:16:19 +0000

    fix: quitando referencia circular

--- myApp.js
+++ myApp.js
@@ -9,13 +9,9 @@ app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: 'deny' }));
 
 app.use(express.static('public'));
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-const api = require('./server.js');
-app.use('/_api', api);
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});
-module.exports = app;
+
+module.exports = app; // <--- ESTO ES LO MÁS IMPORTANTE
